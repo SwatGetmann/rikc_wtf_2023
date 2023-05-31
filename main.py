52b95df49a966b379446fe0d59ceda5ebd82ccf2
SwatGetmann <swatgetmann@gmail.com>
2023-05-31 18:01:16 +0400

    test run

---
+++ main.py
@@ -0,0 +1,14 @@
+"""
+Small test programm to check distribution of test rates for Belarusian Exams.
+Uses:
+* pypdf2
+* pandas
+"""
+
+import pandas as pd
+import numpy as np
+import tabula
+import PyPDF2
+
+if __name__ == "__main__":
+    print("Test Run done well.")
